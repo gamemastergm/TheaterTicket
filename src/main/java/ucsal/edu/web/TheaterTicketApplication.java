76f4ac0abecd2e16150f78fe1905960109e240eb
GameMaster <GameMaster@GameMaster>
2023-08-21 16:29:11 -0300

    Git Init
    Signed-off-by: GameMaster <GameMaster@GameMaster>

---
+++ src/main/java/ucsal/edu/web/TheaterTicketApplication.java
@@ -0,0 +1,13 @@
+package ucsal.edu.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheaterTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucsal/edu/web/TheaterTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package ucsal.edu.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheaterTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
